a0ad7208637f265dc548559490bbf1b750603a6d
Martin Rebane <martin@linuxator.com>
2017-11-24 12:10:17 +0200

    comment example v2

--- JOOP-12/src/storage/TicketStorage.java
+++ JOOP-12/src/storage/TicketStorage.java
@@ -10,9 +10,13 @@ public class TicketStorage {
 	
 	List<BoardingPass> passes = new ArrayList<>();
 	
-	Integer lock = 4;
+	// do not use Integer as lock!
+	Object lock = new Object();
 	
 	public void addTicket(BoardingPass ticket) {
+		// can actually safely use passes instead of lock
+		// no real need to create separte lock
+		// just here to demonstrate the concept
 		synchronized (lock) {
 			passes.add(ticket);
 			lock.notifyAll();
@@ -29,6 +33,7 @@ public class TicketStorage {
 	}
 	
 }
+
 
 
 
