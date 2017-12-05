7cc4f4e9338674325ef6f0916d39df0b12e537b7
Martin Rebane <martin@linuxator.com>
2017-12-05 15:12:40 +0200

    Add comments to CompletableFuture example

--- JOOP-14/src/cards/AsyncCardSystem.java
+++ JOOP-14/src/cards/AsyncCardSystem.java
@@ -7,15 +7,28 @@ public class AsyncCardSystem {
 	Response getCardDetails() throws InterruptedException, ExecutionException {
 		Response r = new Response();
 		
+		/*
+		 * This example makes use of CompletableFuture by defining further action
+		 * 
+		 * */
+		
 		// Promise, ListenableFuture
 		CompletableFuture
+			// job that is done asynchronously
 			.supplyAsync(Details::new)
+			// when this job is finised, do this action with a result
 			.thenAccept(d -> System.out.println(d));
 		
+		
+		/*
+		 * This example acts just like regular Future
+		 * 
+		 * */
 		CompletableFuture<Details> cf = CompletableFuture
-			.supplyAsync(Details::new);
+			.supplyAsync(Details::new); // start async work
 
 		System.out.println("---block---");
+		// cg.get() blocks just like Future, no real use of CompletableFuture here
 		r.setCardDetails(cf.get());
 		
 		CompletableFuture
